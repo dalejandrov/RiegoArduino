bfc2118e7c13b60fae84060245ac072e30630eba
dalejandrov <danielvalenbf@gmail.com>
2021-11-23 15:49:11 -0500

    Cambios aplicados

--- RiegoArduino.ino
+++ RiegoArduino.ino
@@ -238,7 +238,7 @@ menu:
     //  digitalWrite(pinBuzzer, HIGH);
       delay(1000);
       digitalWrite(pinBuzzer, LOW);
-      
+      /*
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("SEBASTIAN MENDEZ");
@@ -251,7 +251,7 @@ menu:
       lcd.print("     GURUPO     ");
       lcd.setCursor(0,1);
       lcd.print("       13       ");
-      delay(2000);
+      delay(2000);*/
 
       lcd.clear();
       lcd.setCursor(0,0);
